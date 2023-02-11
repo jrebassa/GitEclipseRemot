5ebd946994cd73e5eb2e41aa5d897e54a3ef5dd7
Joan <jrebassa@gmail.com>
2023-02-11 20:20:38 +0100

    cinquè commit deposito

--- deposito/src/cuentas/Main.java
+++ deposito/src/cuentas/Main.java
@@ -10,9 +10,6 @@ package cuentas;
  */
 public class Main {
 
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
         CCuenta cuenta1;
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
