484d3520bc9b116cb133b586e430c2ef205515ba
Joan <jrebassa@gmail.com>
2023-02-11 19:32:38 +0100

    quart commit deposito

--- deposito/src/cuentas/CCuenta.java
+++ deposito/src/cuentas/CCuenta.java
@@ -1,5 +1,14 @@
 package cuentas;
 
+/**
+ * Classe que defineix objectes del tipus compte bancari i implementa els mètodes getter i setter
+ * pels seus atributs i els mètodes per ingresar i retirar quantitats del compte.
+ * 
+ * @author Joan Rebassa Oliver
+ * @version 1.0
+ * @since 1.0
+ *
+ */
 public class CCuenta {
 
     private String nombre;
@@ -7,10 +16,22 @@ public class CCuenta {
     private double saldo;
     private double tipoInterés;
 
+    /**
+     * Mètode constructor de sa classe CCuenta per defecte.
+     */
     public CCuenta()
     {
     }
 
+    /**
+     * Mètode constructor de sa classe CCuenta per instanciar objectes inicialitzant els 
+     * seus paràmetres
+     * 
+     * @param nom Nom del titular del compte
+     * @param cue Número del compte 
+     * @param sal Saldo del compte
+     * @param tipo Tipus d'interés
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         setNombre(nom);
@@ -18,11 +39,18 @@ public class CCuenta {
         setSaldo(sal);
     }
 
+    /**
+     * @return Retorna el valor del saldo del compte
+     */
     public double estado()
     {
         return getSaldo();
     }
 
+    /**
+     * @param cantidad Quantitat a ingresar en el compte
+     * @throws Exception El mètode llança una excepció si sa quantitat és menor de 0
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
@@ -30,6 +58,10 @@ public class CCuenta {
         setSaldo(getSaldo() + cantidad);
     }
 
+    /**
+     * @param cantidad Quantitat a retirar del compte
+     * @throws Exception El mètode llança una excepció si sa quantitat és menor o igual a 0
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
@@ -40,56 +72,56 @@ public class CCuenta {
     }
 
 	/**
-	 * @return the nombre
+	 * @return Retorna el nom del titular
 	 */
 	public String getNombre() {
 		return nombre;
 	}
 
 	/**
-	 * @param nombre the nombre to set
+	 * @param nombre Modifica el nom del titular
 	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
 	/**
-	 * @return the cuenta
+	 * @return Retorna el número de compte
 	 */
 	public String getCuenta() {
 		return cuenta;
 	}
 
 	/**
-	 * @param cuenta the cuenta to set
+	 * @param cuenta Modifica el número de compte
 	 */
 	public void setCuenta(String cuenta) {
 		this.cuenta = cuenta;
 	}
 
 	/**
-	 * @return the saldo
+	 * @return Retorna el saldo
 	 */
 	public double getSaldo() {
 		return saldo;
 	}
 
 	/**
-	 * @param saldo the saldo to set
+	 * @param saldo Modifica el saldo
 	 */
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
 
 	/**
-	 * @return the tipoInterés
+	 * @return Retorna el tipus d'interés
 	 */
 	public double getTipoInterés() {
 		return tipoInterés;
 	}
 
 	/**
-	 * @param tipoInterés the tipoInterés to set
+	 * @param tipoInterés Modifica el tipus d'interés
 	 */
 	public void setTipoInterés(double tipoInterés) {
 		this.tipoInterés = tipoInterés;
--- deposito/src/cuentas/Main.java
+++ deposito/src/cuentas/Main.java
@@ -1,13 +1,28 @@
 package cuentas;
 
+/**
+ * Classe per generar i operar amb comptes bancaris
+ * 
+ * @author Joan Rebassa Oliver
+ * @version 1.0
+ * @since 1.0
+ *
+ */
 public class Main {
 
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         CCuenta cuenta1;
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         operativa_cuenta(cuenta1, 0);
     }
 
+	/**
+	 * @param cuenta1 Paràmetre del mètode operativa_cuenta del objecte CCuenta amb el que operar
+	 * @param cantidad Paràmetre del mètode operativa_cuenta amb el valor de sa quantitat
+	 */
 	private static void operativa_cuenta(CCuenta cuenta1, float cantidad) {
 		double saldoActual;
 		double saldoFinal;
